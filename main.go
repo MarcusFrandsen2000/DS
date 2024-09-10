ae26d15042ef35e5c804f737727f2c683da2dde1
Marcus Frandsen <megf@itu.dk>
2024-09-10 10:30:16 +0200

    Initial solution (Unfinished)
    Made fork and philosopher structs to give each the necessary attributes.
    
    Made eat function to lock forks when used and unlocking once finished.
    
    Made a main function to initialize all forks and philosophers and assigning correct leftForks and rightForks to each philosopher.

--- main.go
+++ main.go
@@ -1,1 +1,49 @@
-// Testtesttest
+package main
+
+import {
+	"fmt"
+	"sync"
+	"time"
+}
+
+type fork struct {
+	sync.Mutex
+}
+
+type philosopher struct {
+	id int
+	leftFork, rightFork * fork
+}
+
+func (p philosopher) eat () {
+	p.leftFork.Lock()
+	p.rightFork.Lock()
+
+	fmt.Printf("Philosopher", p.id, "is eating")
+	time.Sleep(10*time.Second)
+
+	p.leftFork.Unlock()
+	p.rightFork.Unlock()
+
+	fmt.Printf("Philosopher", p.id, "is finished eating")
+	time.Sleep(10 * time.Second)
+}
+
+func main() {
+	count := 5
+
+	forks := make([] * fork, count)
+
+	for i := 0; i < count; i++ {
+		forks[i] = new(fork)
+	}
+
+	philosophers := make([] * philosopher, count)
+
+	for i := 0; i < count; i++ {
+		philosophers[i] = &philosopher {
+			id: i, leftFork: forks[i], rightFork: forks[(i+1) % 5],
+		}
+		go philosophers[i].eat()
+	}
+}
