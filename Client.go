50685d3ce97b610eb2f124d2e21a2203c376628f
Marcus Frandsen <megf@itu.dk>
2024-11-23 15:12:21 +0100

    :bee:

--- Client.go
+++ Client.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"math/rand"
 	"time"
-	"sync"
+	"fmt"
 
 	pb "DS/proto"
 
@@ -65,40 +65,58 @@ func (c *Client) getState() (*pb.ResultResponse){
 }
 
 func main(){
-	var wg sync.WaitGroup
+	highestBidderCounter := 0
+	auctionTimeframeLimit := int32(100)
+	// Prompt user to enter a client ID
+	var clientID int32
+	fmt.Print("Enter the client ID (as an integer): ")
 
-	for i := 1; i < 4; i++ {
-		wg.Add(1)
-		clientID := int32(i)
-		go func(clientID int32) {
-			defer wg.Done()
-			client, err := NewClient(clientID, "localhost:50051")
+	// Use fmt.Scan() to get the user input
+	_, err := fmt.Scan(&clientID)
+	if err != nil {
+		log.Fatalf("Error reading client ID: %v", err)
+	}
+	client, err := NewClient(clientID, "localhost:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to create client %d", clientID)
+	}
+
+	for {
+		time.Sleep(time.Duration(5) * time.Second) //Each node waits some time before requesting
+		log.Printf("Client %d is requesting Auction State\n", clientID)
+		status := client.getState()
+
+		// Check if the timeframe has exceeded the limit
+		if status.Timeframe > auctionTimeframeLimit {
+			log.Printf("Client %d: Auction timeframe has exceeded the limit. Stopping bidding.", clientID)
+			break
+		}
+
+		if status.HighestBidder == clientID {
+			log.Printf("Client %d is already the highest bidder", clientID)
+			highestBidderCounter++
+			if highestBidderCounter == 3 {
+				log.Printf("Going once")
+				time.Sleep(time.Duration(2) * time.Second)
+				log.Printf("Going twice")
+				time.Sleep(time.Duration(2) * time.Second)
+				log.Printf("SOOOOLD to Client %d for %d dollars", status.HighestBidder, status.HighestBid)
+				break
+			}
+		} else if client.money <= status.HighestBid {
+			log.Printf("Client %d doesnt have enough money to place a higher bid", clientID)
+			break
+		} else {
+			bidResp, err := client.placeBid(status.HighestBid + 1)
 
 			if err != nil {
-				log.Fatalf("Failed to create client %d", clientID)
+				log.Fatalf("Error placing bid: %v", err)
+				return
 			}
 
-			for {
-                time.Sleep(time.Duration(5) * time.Second) //Each node waits some time before requesting
-                log.Printf("Client %d is requesting Auction State\n", clientID)
-				status := client.getState()
-
-				if status.HighestBidder == clientID {
-					log.Printf("Client %d is already the highest bidder", clientID)
-				} else if client.money <= status.HighestBid {
-					log.Printf("Client %d doesnt have enough money to place a higher bid", clientID)
-				} else {
-					bidResp, err := client.placeBid(status.HighestBid + 1)
-
-					if err != nil {
-						log.Fatalf("Error placing bid: %v", err)
-						return
-					}
-
-					log.Printf("%sClient %d bid %d", bidResp.Message, clientID, bidResp.Bid)
-				}
-            }
-		}(clientID)
+			log.Printf("%sClient %d bid %d", bidResp.Message, clientID, bidResp.Bid)
+			highestBidderCounter = 0
+		}
 	}
-	wg.Wait()
 }
